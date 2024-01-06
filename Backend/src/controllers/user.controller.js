3e286c35d855647485bb0f3055f366982a52e04d
Amanastel <87702404+Amanastel@users.noreply.github.com>
2024-01-06 17:48:06 +0530

    refresh token and access token

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -35,6 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
     // return res
 
 
+    
     const { fullName, email, username, password } = req.body
     // console.log("email: ", email);
 
